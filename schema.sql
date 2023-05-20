a169e120fd107af088315b29a75094c3448f85c5
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-05-20 22:45:14 +0600

    Add query for multiple tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,18 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species varchar;
+
+CREATE TABLE owners (
+  id serial PRIMARY KEY,
+  full_name varchar,
+  age int
+);
+
+CREATE TABLE species (
+  id serial PRIMARY KEY,
+  name varchar
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id int REFERENCES species(id);
+ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
